2986a85e621e5ecc80d983e311be6afb25b5dbbe
Ellie7 <emartell1@sheffield.ac.uk>
2017-10-10 16:23:49 +0100

    population projections section of Crouse 1987 methods underway

--- Crouse.R
+++ Crouse.R
@@ -1,4 +1,23 @@
 #Crouse et al. 1987 
 #A STAGE-BASED POPULATION MODEL FOR LOGGERHEAD SEA TURTLES AND IMPLICATIONS FOR CONSERVATION
-
-A <- matrix(c(0, 0, 0, 0, 127, 4, 80, 0.6747, 0.7370, 0, 0,0, 0, 0, 0, 0.0486, 0.6610, 0, 0, 0, 0, 0, 0, 0.0147, 0.6907, 0, 0, 0, 0, 0, 0, 0.0518, 0, 0, 0, 0, 0, 0, 0, 0.8091, 0, 0, 0, 0, 0, 0, 0, 0.8091, 0.8089), nr=7, byrow = TRUE)
+#recreating table 4
+A <- matrix(c(0, 0, 0, 0, 127, 4, 80, 0.6747, 0.7370, 0, 0,0, 0, 0, 0, 0.0486, 0.6610, 0, 0, 
+              0, 0, 0, 0, 0.0147, 0.6907, 0, 0, 0, 0, 0, 0, 0.0518, 0, 0, 0, 0, 0, 0, 0, 0.8091, 
+              0, 0, 0, 0, 0, 0, 0, 0.8091, 0.8089), nr=7, byrow = TRUE) 
+#population projections 
+#eigen analysis 
+eigs.A <- eigen(A)
+eigs.A
+#finding the first eigenvalue (finite rate of increase)
+dom.pos <- which.max(eigs.A[["values"]])
+L1 <- Re(eigs.A[["values"]][dom.pos])
+L1
+#=0.945031
+#calculating the stable stage distribution 
+w <- Re(eigs.A[["vectors"]][,dom.pos])
+ssd <- w/sum(w)
+round(ssd,3)
+#0.207 0.670 0.115 0.007 0.000 0.000 0.002 
+#calculating the reproductive value 
+M <- eigen(t(A))
+v <- Re(M$vectors[])
