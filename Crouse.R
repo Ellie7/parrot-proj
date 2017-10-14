f78e9165f84b357339ab53e393b0bdfbcbf757e0
Ellie7 <emartell1@sheffield.ac.uk>
2017-10-14 12:24:55 +0100

    -install knitr to create tables for r markdown documents -used to knitr on table 5

--- Crouse.R
+++ Crouse.R
@@ -2,10 +2,10 @@
 #A STAGE-BASED POPULATION MODEL FOR LOGGERHEAD SEA TURTLES AND IMPLICATIONS FOR CONSERVATION
 install.packages("dplyr")
 install.packages("ggplot2")
-install.packages("xtable")
+install.packages("knitr")
 library(dplyr)
-library(ggplot2)
-library(xtable)
+library(ggplot2) 
+library(knitr)
 table.3 <- read.csv("~/1 UNIVERSITY/Level 4/Project & Dissertation/Crouse 1987/table 3 from crouse.csv")
 table.3
 #----------------- Creating a stage-based projection matrix, for each stage, 
@@ -94,6 +94,9 @@ RV
 #to create table 5 (from Crouse 1987)
 tab <- select(table.3, stage_number, class)
 tab_5<- data.frame(tab, stable, RV)
+colnames(tab_5) <- c("Stage number", "Stage Class", "Stable stage distribution (Dominant eigenvector)", "Reproductive values (left eigenvector)" )
+tab_5
+kable(tab_5, caption = "Table 5. Stable stage distribution (wJ) and reproductive values (v') for the loggerhead population matrix given in Table 4.")
 
 #---------------------------------- sensitivity analyses 
 #sensitivity of projection matrices 
