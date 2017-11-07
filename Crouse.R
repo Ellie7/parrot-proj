805d765ea25b1778a728289bc71ce12163d73a10
Ellie7 <emartell1@sheffield.ac.uk>
2017-11-07 18:53:19 +0000

    - the numbers for figure 1a weren't wrong but the bars were in the wrong order, fixed in the plotting by using scale_x_discrete

--- Crouse.R
+++ Crouse.R
@@ -265,4 +265,5 @@ table_decrease <- data.frame(stage_class, rs)
 graph <- ggplot(table_decrease, aes(x = stage_class, y = rs)) + geom_bar(stat = "identity")
 graph1 <- graph + labs(x = "Stage Class", y = "Intrinsic rate of Increase (r)") + geom_hline(aes(yintercept=-0.056399), linetype = "dashed", size = 0.75)  
 graph2 <- graph1 + theme(panel.grid.minor=element_blank(), panel.grid.major=element_blank())+ expand_limits(y = -0.3, x = 0.4)
-figure1a <- graph2 + theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) 
+figure1a <- graph2 + theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) + scale_x_discrete(limits = c("Fecundity", "Eggs/Hatchlings", "Small Juveniles", "Large Juveniles", "Subadults", "Novice Breeders", "1st-yr Remigrants", "Mature Breeders"))
+figure1a 
