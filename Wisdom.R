f964d80a40ae3c0b4728e72cfaea4131219dabda
Ellie7 <emartell1@sheffield.ac.uk>
2018-01-23 12:20:49 +0000

    - starting wisdom script

---
+++ Wisdom.R
@@ -0,0 +1,8 @@
+# Wisdom et al. 2000 
+# life stage simulation analysis: estimating vital-rate effects on population growth for conservation 
+library(dplyr)
+library(ggplot2) 
+library(knitr)
+library(primer)
+library(Rmisc)
+library(agricolae) 
