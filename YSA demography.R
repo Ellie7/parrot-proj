a85316ebce938f0d985af422513c65e0a32338b9
Ellie7 <emartell1@sheffield.ac.uk>
2018-03-08 15:14:34 +0000

    - starting new script for YSA demography - packages are ones used in wisdom script

---
+++ YSA demography.R
@@ -0,0 +1,13 @@
+# Yellow-shouldered amazon  
+rm(list=ls())
+library(dplyr)
+library(ggplot2) 
+library(knitr)
+library(primer)
+library(Rmisc)
+library(agricolae)
+library(popbio)
+library(MASS) 
+YSA_demog_data_master <- read.csv("YSA_demog_data_master.csv")
+
+
