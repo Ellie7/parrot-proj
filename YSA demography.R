de1c6c02d7a9ae737e08b1df77b7b28a7e44e002
Ellie7 <emartell1@sheffield.ac.uk>
2018-03-12 11:00:52 +0000

    - matrix structure

--- YSA demography.R
+++ YSA demography.R
@@ -14,25 +14,23 @@ ysa <- YSA_demog_data_master
 
 
 ysaFunc <- function (ysa) 
-{ sc1 <- betaval((chicken$s[1]), 0.129, fx=runif(1)) #0.129 is mean standard deviation for stage 1, didn't know how to correctly calculate ssd for
-sc2 <- betaval((chicken$s[5]), (chicken$ssd[5]), fx=runif(1)) # this stage as the survival is from the multiplication of  s1a, s1b and s1c
-sc3 <- betaval((chicken$s[6]), (chicken$ssd[6]), fx=runif(1))
-sc4 <- betaval((chicken$s[7]), (chicken$ssd[7]), fx=runif(1))
-#m 
-mc2 <- rnorm(1, mean = (chicken$m[5]), sd = (chicken$msd[5]))
-mc3 <- rnorm(1, mean = (chicken$m[6]), sd = (chicken$msd[6]))
-mc4 <- rnorm(1, mean = (chicken$m[7]), sd = (chicken$msd[7]))
-mc5 <- rnorm(1, mean = (chicken$m[7]), sd = (chicken$msd[7]))
+{ #ps
+  p1 <- betaval((chicken$s[1]), 0.129, fx=runif(1)) #0.129 is mean standard deviation for stage 1, didn't know how to correctly calculate ssd for
+p2 <- betaval((chicken$s[5]), (chicken$ssd[5]), fx=runif(1)) # this stage as the survival is from the multiplication of  s1a, s1b and s1c
+p3 <- betaval((chicken$s[6]), (chicken$ssd[6]), fx=runif(1))
+#f
+f3 <- rnorm(1, mean = (chicken$m[7]), sd = (chicken$msd[7]))
+#g 
+g1 <- 
+g2 <- 
 matrix2 <- matrix(0, nrow = 4, ncol = 4)
-#add sxmx
-matrix2[1,1] <- (sc1*mc2) 
-matrix2[1,2] <- (sc2*mc3)
-matrix2[1,3] <- (sc3*mc4)
-matrix2[1,4] <- (sc4*mc5)
-#add s(mxc$m[7]))
-matrix2[2,1] <- sc1
-matrix2[3,2] <- sc2
-matrix2[4,3] <- sc3
-matrix2[4,4] <- sc4
+#add first row
+matrix2[1,1] <- (p1a*p1b*p1c) 
+matrix2[1,2] <- (f2)
+matrix2[1,3] <- (f3)
+#add p
+matrix2[2,1] <- g1
+matrix2[3,2] <- p2
+matrix2[4,3] <- p3
 return(matrix2)
 }
