ebf4ba9839f6204f08cf7f99ee37197434c1d6bf
Ellie7 <emartell1@sheffield.ac.uk>
2018-03-14 13:59:33 +0000

    - mistaken , now +

--- YSA demography.R
+++ YSA demography.R
@@ -183,7 +183,7 @@ rs <- log(lambdas)
 class <- c("fecundity", "egg", "nestling", "fledgling", "juvenile", "adult")
 table_decrease <- data.frame(class, rs)
 graph <- ggplot(table_decrease, aes(x = class, y = rs)) + geom_bar(stat = "identity")
-graph1 <- graph + labs(x = "Stage Class", y = "Intrinsic rate of Increase (r)"), linetype = "dashed", size = 0.75)  
+graph1 <- graph + labs(x = "Stage Class", y = "Intrinsic rate of Increase (r)") + linetype = "dashed", size = 0.75)  
 graph2 <- graph1 + theme(panel.grid.minor=element_blank(), panel.grid.major=element_blank())+ expand_limits(y = -0.25, x = 0.2)
 figure1a <- graph2 + theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) + scale_x_discrete(limits = c("Fecundity", "Eggs/Hatchlings", "Small Juveniles", "Large Juveniles", "Subadults", "Novice Breeders", "1st-yr Remigrants", "Mature Breeders"))
 figure1a 
