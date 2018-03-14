afd528d2c04b91dc507a963d156d4ae068fbec19
Ellie7 <emartell1@sheffield.ac.uk>
2018-03-14 15:54:54 +0000

    - troubleshooting function - accounting for 1a/b/c thing in yellowFunc

--- YSA demography.R
+++ YSA demography.R
@@ -41,20 +41,19 @@ outMats <- map(yellowMats, function(x) ysaFunc(x)) # makes 10 matrices
 eigenOuts <- map(outMats, function(x) eigen(x))
 
 
-
-
 ysaFunc <- function (dataSource) 
 { 
 #ps
-p1a<- betaval(), (), fx=runif(1)) 
- 
-p2 <- betaval(), (), fx=runif(1)) 
-p3 <- betaval(), (), fx=runif(1))
+p1a<- betaval((0.89), (0.06), fx=runif(1)) 
+p1b<- betaval((0.78), (0.07), fx=runif(1))
+p1c<- betaval((0.73), (0.07), fx=runif(1)) #0.07 as filler
+p2 <- betaval((0.925), (0.025), fx=runif(1)) # 0.05 or 0.025?
+p3 <- betaval((0.925), (0.025), fx=runif(1))
 #f
-f3 <- rnorm(1, mean = (0.33), sd = ()) #should 0.33 be divided by 2? because its the percentage of females 
+f3 <- rnorm(1, mean = (0.33), sd = (0.3)) #should 0.33 be divided by 2? because its the percentage of females #0.3 is random atm 
 #g 
-g1 <- 
-g2 <- 
+Pi <- ((1 - (pi^(di - 1)))/(1 - (pi^di)))*pi
+Gi <- (pi^di*(1 - pi))/(1 - pi^di)
 matrix2 <- matrix(0, nrow = 4, ncol = 4)
 #add ps 
 matrix2[1,1] <- (p1a*p1b*p1c)# this stage as the survival is from the multiplication of  p1a, p1b and p1c
@@ -63,10 +62,13 @@ matrix2[3,3] <- p3
 #add f
 matrix2[1,3] <- (f3)
 #add gs 
-matrix2[2,1] <- g1
+matrix2[2,1] <- Gi[]
 matrix2[3,2] <- g2
 return(matrix2)
 } 
+
+ysaFunc()
+
 
 ############ for now 
 
@@ -79,12 +81,14 @@ p1c <- Pi[3]
 p2 <- Pi[4] 
 p3 <- Pi[5]
 f3 <- F[5]
-g1 <- Gi[1]
-g2 <- Gi[2]
+p1 <- p1a*p1b*p1c
+d1 <- di[1]+di[2]+di[3] #because of 1a 1b 1c thing
+g1 <- (p1^d1*(1 - p1))/(1 - p1^d1) #because of 1a 1b 1c thing
+g2 <- Gi[4]
 #matrix structure
 matrix2 <- matrix(0, nrow = 3, ncol = 3)
 #add ps 
-matrix2[1,1] <- (p1a*p1b*p1c)# this stage as the survival is from the multiplication of  p1a, p1b and p1c
+matrix2[1,1] <- p1# this stage as the survival is from the multiplication of  p1a, p1b and p1c
 matrix2[2,2] <- p2
 matrix2[3,3] <- p3
 #add f
