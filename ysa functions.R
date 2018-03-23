c68d895ac7a547e39327220a9c4a10eae532bd03
Ellie7 <emartell1@sheffield.ac.uk>
2018-03-23 16:14:16 +0000

    - updating with andrews comments

--- ysa functions.R
+++ ysa functions.R
@@ -13,29 +13,40 @@ library(tidyverse)
 ysaFunc <- function (dataSource) 
 { 
   #ps
-  p1a<- betaval((pi[1]), (piSD[1]), fx=runif(1)) 
-  p1b<- betaval((pi[2]), (piSD[2]), fx=runif(1)) 
-  p1c<- betaval((pi[3]), (piSD[3]), fx=runif(1))
-  p2 <- betaval((pi[4]), (piSD[4]), fx=runif(1))
-  p3 <- betaval((pi[5]), (piSD[5]), fx=runif(1))
+  p1a<- betaval((dataSource$pi[1]), (dataSource$piSD[1]), fx=runif(1)) 
+  p1b<- betaval((dataSource$pi[2]), (dataSource$piSD[2]), fx=runif(1)) 
+  p1c<- betaval((dataSource$pi[3]), (dataSource$piSD[3]), fx=runif(1))
+  p2 <- betaval((dataSource$pi[4]), (dataSource$piSD[4]), fx=runif(1))
+  p3 <- betaval((dataSource$pi[5]), (dataSource$piSD[5]), fx=runif(1))
+  
   #f
-  f3 <- rnorm(1, mean = (f[5]), sd = (fSD[5])) #should 3.3 be divided by 2  
+  f3 <- rnorm(1, mean = (dataSource$f[5]), sd = (dataSource$fSD[5])) #should 3.3 be divided by 2  
+  
   # Pi <- ((1 - (pi^(di - 1)))/(1 - (pi^di)))*pi ------- equation for Pi's
   # Gi <- (pi^di*(1 - pi))/(1 - pi^di)           ------- equation for Gi's
-  matrix2 <- matrix(0, nrow = 3, ncol = 3)
-  d1 <- di[1] + di[2] + di[3]
-  d2 <- di[4]
-  d3 <- di[5]
+  
+  #d
+  d1 <- dataSource$di[1] + dataSource$di[2] + dataSource$di[3]
+  d2 <- dataSource$di[4]
+  d3 <- dataSource$di[5]
+  
+  # this uses p1's defined above
   p1 <- (p1a*p1b*p1c) # this stage as the survival is from the multiplication of  p1a, p1b and p1c
   #add ps 
+  
+  # construct the matrix using defined parameters above
+  matrix2 <- matrix(0, nrow = 3, ncol = 3)
   matrix2[1,1] <- ((1 - (p1^(d1 - 0.99)))/(1 - (p1^d1)))*p1 #0.99 as doesn't lie 1s 
   matrix2[2,2] <- ((1 - (p2^(d2 - 1)))/(1 - (p2^d2)))*p2
   matrix2[3,3] <- ((1 - (p3^(d3 - 1)))/(1 - (p3^d3)))*p3
+  
   #add f
   matrix2[1,3] <- f3
+  
   #add gs 
-  matrix2[2,1] <- (p1^d1*(1 - p1))/(1 - p1^d1) # G1 #hope correct biology - is G1 appropriate  
-  matrix2[3,2] <- (p2^d2*(1 - p2))/(1 - p2^d2) # G2 #hope correct biology
+  matrix2[2,1] <- (p1^d1*(1 - p1))/(1 - p1^d1) 
+  matrix2[3,2] <- (p2^d2*(1 - p2))/(1 - p2^d2) 
+  
   return(matrix2)
 } 
 
