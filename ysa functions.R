45525199d3f4d944732e8a65a41d006203748d1c
Ellie7 <emartell1@sheffield.ac.uk>
2018-03-24 13:43:23 +0000

    - labelling function script

--- ysa functions.R
+++ ysa functions.R
@@ -13,10 +13,7 @@ library(tidyverse)
 # Here is your data that will input into the function
 yellow <- data_frame(stage, class, di, pi, piSD,  f, fSD)
 
-# Here is your function
-# pi, piSD etc are inside the data frame.  You need to specify this
-# for example: dataSource$pi[1] is where pi[1] is located
-
+# function 
 ysaFunc <- function (dataSource) 
 { 
   #ps
