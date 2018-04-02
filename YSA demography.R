b3cb8881e7f79120ae47f16553352349142f0f82
Ellie7 <emartell1@sheffield.ac.uk>
2018-04-02 15:18:21 +0100

    - playing around with effects of different lambdas

--- YSA demography.R
+++ YSA demography.R
@@ -35,6 +35,7 @@ mat1 <- map(1:10, function(x) ysaFunc(yellow))
 mat2<-map(mat1, function(x) eigen(x))
 
 A <- ysaFunc(yellow)
+A
 #eigen analysis 
 eigs.A <- eigen(A)
 eigs.A
@@ -178,8 +179,38 @@ fig.3 + scale_x_discrete(limits=c("E","J","A"))
 ############################################################## EFFECTS OF PER CAPITA GROWTH RATE (effects of different lambda)
 mat2
 #finding the first eigenvalue (finite rate of increase)
-dom.pos <- which.max(mat2[["values"]])
-Lambdas <- Re(mat2[["values"]][dom.pos])
-Lambdas
-Lambdas1 <- Re(mat2$values[1])
+B <- ysaFunc(yellow) #different matrices generated each time for now 
+C <- ysaFunc(yellow)
+D <- ysaFunc(yellow)
+E <- ysaFunc(yellow)
+F <- ysaFunc(yellow)
+
+eigs.B <- eigen(B)
+eigs.C <- eigen(C)
+eigs.D <- eigen(D)
+eigs.E <- eigen(E)
+eigs.F <- eigen(F)
+
+bdom.pos <- which.max(eigs.B[["values"]])
+cdom.pos <- which.max(eigs.C[["values"]])
+ddom.pos <- which.max(eigs.D[["values"]])
+edom.pos <- which.max(eigs.E[["values"]])
+fdom.pos <- which.max(eigs.F[["values"]])
+
+Lb <- Re(eigs.B[["values"]][bdom.pos])
+Lc <- Re(eigs.C[["values"]][cdom.pos])
+Ld <- Re(eigs.D[["values"]][ddom.pos])
+Le <- Re(eigs.E[["values"]][edom.pos])
+Lf <- Re(eigs.F[["values"]][fdom.pos])
+
+N0 <- 100
+time <- 0:10
+lambdas <- c(Lb, Lc, Ld, Le, Lf, LFi, L1ai, L1bi, L1ci, L2i, L3i)
+#use sapply to apply geometrix growth function to each lambda, x stands for each lambda, which the funcion uses to 
+# calculate population size
+N.all <-sapply(lambdas, function(x) N0*x^time)
+matplot(time, N.all, xlab="Years", ylab="N", pch = 1:3)
+abline(h=N0, lty = 3)
+text(0.5, 250, expression(lamda > 1), cex=1.2)
+text(0.5, 20, expression(lamda < 1), cex=1.2)
 
