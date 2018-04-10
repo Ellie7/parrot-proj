cfdb0fd2e942e43d323656dc64484d0876e79eeb
Ellie7 <emartell1@sheffield.ac.uk>
2018-04-10 10:29:00 +0100

    - making this script exclusively elasticity & sensitivity analyses

--- YSA demography.R
+++ YSA demography.R
@@ -1,4 +1,4 @@
-# Yellow-shouldered amazon  
+# Yellow-shouldered amazon elasticity analyses 
 rm(list=ls())
 library(dplyr)
 library(ggplot2) 
@@ -176,38 +176,5 @@ sens <- read.csv("cheat for now.csv")
 fig.3 <- ggplot(sens, aes(x = Stage, y = Elasticity, colour = Vital_rate, Vital_rate)) + geom_line() + geom_point(size = 4) + labs(x = "Stage", y = "Elasticity")
 fig.3 + scale_x_discrete(limits=c("E","J","A")) 
 
-############################################################## EFFECTS OF PER CAPITA GROWTH RATE (effects of different lambda)
-mat2
-#finding the first eigenvalue (finite rate of increase)
-B <- ysaFunc(yellow) #different matrices generated each time for now 
-C <- ysaFunc(yellow)
-D <- ysaFunc(yellow)
-E <- ysaFunc(yellow)
-F <- ysaFunc(yellow)
 
-eigs.B <- eigen(B)
-eigs.C <- eigen(C)
-eigs.D <- eigen(D)
-eigs.E <- eigen(E)
-eigs.F <- eigen(F)
-
-bdom.pos <- which.max(eigs.B[["values"]])
-cdom.pos <- which.max(eigs.C[["values"]])
-ddom.pos <- which.max(eigs.D[["values"]])
-edom.pos <- which.max(eigs.E[["values"]])
-fdom.pos <- which.max(eigs.F[["values"]])
-
-Lb <- Re(eigs.B[["values"]][bdom.pos])
-Lc <- Re(eigs.C[["values"]][cdom.pos])
-Ld <- Re(eigs.D[["values"]][ddom.pos])
-Le <- Re(eigs.E[["values"]][edom.pos])
-Lf <- Re(eigs.F[["values"]][fdom.pos])
-
-N0 <- 100
-time <- 0:10
-lambdas <- c(Lb, Lc, Ld, Le, Lf, LFi, L1ai, L1bi, L1ci, L2i, L3i)
-#use sapply to apply geometrix growth function to each lambda, x stands for each lambda, which the funcion uses to 
-# calculate population size
-N.all <-sapply(lambdas, function(x) N0*x^time)
-matplot(time, N.all, xlab="Years", ylab="N", pch = 1:3)
 
