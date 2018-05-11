12d458518748d9800879c3971a4e6161afe8617f
Ellie7 <emartell1@sheffield.ac.uk>
2018-05-11 09:53:54 +0100

    - using map() to generate multiple matrices - using map() to generat the eigen system for multiple matrices

--- YSA LSA.R
+++ YSA LSA.R
@@ -12,4 +12,11 @@ library(tidyverse)
 source(file = "ysa functions.R")
 source(file = "ysa demography.R") 
 
+# making matrices using 'ysaFunc' function 
 ysaFunc(yellow)
+yellow
+#make 10 matrices of random numbers.#for current code these aren't random and are drawn from ysa beta and normal distributed 
+#vital rates
+mat1 <- map(1:10, function(x) ysaFunc(yellow))
+# use these matrices in mat 1 and get the eigen system for each....
+mat2<-map(mat1, function(x) eigen(x))
