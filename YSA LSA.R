898fd7f2c9bb1f05c1aee5f322de9d00dbfca4e6
Ellie7 <emartell1@sheffield.ac.uk>
2018-05-14 18:05:35 +0100

    - code for creating a dataframe where columns are the matrix ID and rows are elements - for extracting list elements for graphs - with hudsonia code need to convert to ysa

--- YSA LSA.R
+++ YSA LSA.R
@@ -30,26 +30,49 @@ mat3 <- map(mat2, function(x) {
 #rs 
 mat4 <- map(mat3, function(x) log(x)) 
 
-##### alternatively 
+##### alternatively use map and eigen.analysis 
 analysis <- map(mat1, eigen.analysis) 
-#rs
-map(analysis, function(x) log(x$lambda1))
-
-elasticities <- map(analysis, function(x) x$elasticities)
-
-map(elasticities, function(x) 
-  {G1 <- x[2,1] 
-  G2 <- x[3,2] 
-  P2 <- x[2,2] 
-  P3 <- x[3,3] 
-  F3 <- x[1,3]})
-
-
-ggplot(elasticities, aes(y = mean(elasticities)))
 
        
 n0 <- c(1, 1, 1)
 stoch.projection(mat1, n0, tmax = 50, nreps = 10, prob = NULL,
                         nmax = NULL, sumweight = rep(1, length(n0)), verbose=FALSE)
+# load hudsonia data (list of 4 matrices)
+data('hudsonia')
+# use map and eigen.analysis
+out<-map(hudsonia, eigen.analysis)
+
+# the eigen analysis outputs for each matrix
+out
+
+# need this to get the names onto the matrix at the end....
+# only for old skool barplotting.
+# for hudsonia, its seed, seedlings, tiny etc....
+use.names <- rownames(out[[1]]$sensitivities)
+
+# create a data frame where columns are the matrix ID and rows are the elements
+# of the sensitivity matrix
+
+out2 <- map_df(out, function(x) {cbind(c(x$sensitivities))})
+
+# use rowMeans to get the average sensitivity for each element
+# and the se_fnc to get the se
+# and re-create as a matrix
+
+se_fnc <- function(x){sd(x)/sqrt(sum(!is.na(x)))}
+
+# these are the mean and se matrices of sensitivity, in this case.
+mean_sensitivity <- matrix(rowMeans(out2),6,6,
+                           dimnames = list(use.names,use.names))
+se_sensitivity <- matrix(apply(out2, 1, function(x) se_fnc(x)), 6,6)
+
+# ggplot figure making
+g_plot_df <- data.frame(expand.grid(stage_A = use.names, stage_B = use.names),
+                        mean_sens = c(mean_sensitivity), se_sens = c(se_sensitivity))
+
+ggplot(g_plot_df, aes(x = stage_A, y = mean_sens, group = stage_B, fill = stage_B))+
+  geom_col()+
+  facet_wrap(~stage_B)
+
 
     
