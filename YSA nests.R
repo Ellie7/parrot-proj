db5d963a8128bd904ae834c25f766cf78a2746c8
Ellie7 <emartell1@sheffield.ac.uk>
2018-05-18 13:16:25 +0100

    - making the function

--- YSA nests.R
+++ YSA nests.R
@@ -26,14 +26,55 @@ source(file = "YSA life history data.R")
 # N > M  -> (M*F)/N
 # N <= M ->  F
 
+# making the function 
+ysaFuncDD <- function (dataSource, n, threshold) 
+{ 
+  #ps
+  p1a<- betaval((dataSource$pi[1]), (dataSource$piSD[1]), fx=runif(1)) 
+  p1b<- betaval((dataSource$pi[2]), (dataSource$piSD[2]), fx=runif(1)) 
+  p1c<- betaval((dataSource$pi[3]), (dataSource$piSD[3]), fx=runif(1))
+  p2 <- betaval((dataSource$pi[4]), (dataSource$piSD[4]), fx=runif(1))
+  p3 <- betaval((dataSource$pi[5]), (dataSource$piSD[5]), fx=runif(1))
+  
+  #f
+  f3 <- rnorm(1, mean = (dataSource$f[5]), sd = (dataSource$fSD[5])) 
+  
+  # Pi <- ((1 - (pi^(di - 1)))/(1 - (pi^di)))*pi ------- equation for Pi's
+  # Gi <- (pi^di*(1 - pi))/(1 - pi^di)           ------- equation for Gi's
+  
+  #d
+  d1 <- dataSource$di[1] + dataSource$di[2] + dataSource$di[3]
+  d2 <- dataSource$di[4]
+  d3 <- dataSource$di[5]
+  
+  # this uses p1's defined above
+  p1 <- (p1a*p1b*p1c) # this stage as the survival is from the multiplication of  p1a, p1b and p1c
+  #add ps 
+  
+  # construct the matrix using defined parameters above
+  matrix2 <- matrix(0, nrow = 3, ncol = 3)
+  dimnames(matrix2) <- list(rownames(matrix2, do.NULL = FALSE, prefix = "row"),
+                            colnames(matrix2, do.NULL = FALSE, prefix = "col"))
+  matrix2[1,1] <- ((1 - (p1^(d1 - 1)))/(1 - (p1^d1)))*p1 
+  matrix2[2,2] <- ((1 - (p2^(d2 - 1)))/(1 - (p2^d2)))*p2
+  matrix2[3,3] <- ((1 - (p3^(d3 - 1)))/(1 - (p3^d3)))*p3
+  
+  #add f
+  matrix2[1,3] <- f3
+  
+  #add gs 
+  matrix2[2,1] <- (p1^d1*(1 - p1))/(1 - p1^d1) 
+  matrix2[3,2] <- (p2^d2*(1 - p2))/(1 - p2^d2) 
+  
+  return(matrix2)
+} 
+
+# using the function 
 threshold <- 500 # limiting number of nests 
 n <- c(100, 100, 100, 100, 100) # population vector 
-  
-ysaFunc <- function(dataSource)
-ysaFuncDD <- function (dataSource, n, threshold)
-  
+
 A <- ysaFuncDD(yellow)  
 
-n <- A %*% n
+n <- A %*% n 
 
 
